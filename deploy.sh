05a3c4cef70f1194bd7f75466a8644abfcf202d3
Omar Mehawed <omarmehawed4@gmail.com>
2026-02-16 10:28:49 +0200

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-# 1. Check Maintenance Mode (Direct check)
-if [ "$MAINTENANCE_MODE" = "true" ]; then
+# Make sure this file has executable permissions, run `chmod +x deploy.sh` to ensure it does
+
+# Variable name to check maintenance mode
+ENV_VAR_NAME="MAINTENANCE_MODE"
+
+# Check if the environment variable is set to "true"
+if [[ "${!ENV_VAR_NAME}" = "true" ]]; then
   echo "Entering maintenance mode..."
   php artisan down
 fi
 
-# 2. Build Assets
+# Build assets using NPM
 npm run build
 
-# 3. Clear Cache
+# Clear cache
 php artisan optimize:clear
 
-# 4. Cache Config & View ONLY (No Route Cache)
+# Cache the various components of the Laravel application
 php artisan config:cache
 php artisan event:cache
-# php artisan route:cache  <-- (Disabling route cache to avoid duplicates)
+php artisan route:cache
 php artisan view:cache
 
-# 5. Run Migrations
+# Run any database migrations
 php artisan migrate --force
-php artisan db:seed --force
-# 6. Exit Maintenance Mode
-if [ "$MAINTENANCE_MODE" != "true" ]; then
+
+# Check if the environment variable is set to "false" or not set at all
+if [[ "${!ENV_VAR_NAME}" = "false" ]] || [[ -z "${!ENV_VAR_NAME}" ]]; then
   echo "Exiting maintenance mode..."
   php artisan up
 fi
